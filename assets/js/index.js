146c6648e1237d68a94be479da04db780099a302
Florencia Hidalgo Alarcón <f.hidalgoalarcon@gmail.com>
2024-02-22 20:17:43 -0300

    Al hacer click en la img del card el resto del contenido del card se esconde.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,45 @@
 //Toggle
-    //Card avion
-$(document).ready(function(){
-    $("#avion").click(function(){
-      $("#avion-p").toggle();
-    });
+//Card avion
+$(document).ready(function () {
+  $("#avion").click(function () {
+    $("#avion-p").toggle();
+  });
 });
-    //Card montaña
-$(document).ready(function(){
-    $("#mountain").click(function(){
-      $("#mountain-p").toggle();
-    });
+//Card montaña
+$(document).ready(function () {
+  $("#mountain").click(function () {
+    $("#mountain-p").toggle();
+  });
 });
-    //Card camino
-$(document).ready(function(){
-    $("#camino").click(function(){
-      $("#camino-p").toggle();
-    });
+//Card camino
+$(document).ready(function () {
+  $("#camino").click(function () {
+    $("#camino-p").toggle();
+  });
+});
+
+//SlideToggle
+//Card 1
+$(document).ready(function () {
+  $("#img-card-1").click(function () {
+    $("#card-body-1").slideToggle("slow");
+  });
+});
+//Card 2
+$(document).ready(function () {
+  $("#img-card-2").click(function () {
+    $("#card-body-2").slideToggle("slow");
+  });
+});
+//Card 3
+$(document).ready(function () {
+  $("#img-card-3").click(function () {
+    $("#card-body-3").slideToggle("slow");
+  });
+});
+//Card 4
+$(document).ready(function () {
+  $("#img-card-4").click(function () {
+    $("#card-body-4").slideToggle("slow");
+  });
 });
