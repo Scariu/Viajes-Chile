0f7403e33e3fb1cfd96a290e72a510f60955afe2
Florencia Hidalgo Alarcón <f.hidalgoalarcon@gmail.com>
2024-02-22 20:08:43 -0300

    Al hacer click en el icono de la sección presentación este esconde su respectivo parrafo.

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+//Toggle
+    //Card avion
+$(document).ready(function(){
+    $("#avion").click(function(){
+      $("#avion-p").toggle();
+    });
+});
+    //Card montaña
+$(document).ready(function(){
+    $("#mountain").click(function(){
+      $("#mountain-p").toggle();
+    });
+});
+    //Card camino
+$(document).ready(function(){
+    $("#camino").click(function(){
+      $("#camino-p").toggle();
+    });
+});
